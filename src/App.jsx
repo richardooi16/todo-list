535f285232cc0e92b1fa80b06b7a94ddf5e84ff4
bdyee <rooiwork16@gmail.com>
2023-05-15 00:59:32 +0800

    WIP form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from 'react'
+import { useForm } from "react-hook-form";
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [taskList, setTaskList] = useState([]);
+  const [task, setTask] = useState("");
+  const { register, handleSubmit, errors } = useForm();
+
+  const submitTask = async (data) => {
+    console.log(data);
+    taskList.push(data);
+  };
 
   return (
     <div className="App">
@@ -15,7 +24,19 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+
+      <form onSubmit={handleSubmit(submitTask)}>
+        <input type="text" />
+        <button type="submit">Submit</button>
+      </form>
+
+      <div>
+        {taskList.map((data) => (
+          <p>{data}</p>
+        ))}
+      </div>
+
+      {/* <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,9 +47,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </div>
-  )
+  );
 }
 
 export default App
