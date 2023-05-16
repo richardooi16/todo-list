7be6912df98bc7d9659831bd339f07ec0fa0ae59
richardooi16 <rooiwork16@gmail.com>
2023-05-16 13:04:58 +0800

    added form function

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,26 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
   const [taskList, setTaskList] = useState([]);
   const [task, setTask] = useState("");
-  const { register, handleSubmit, errors } = useForm();
 
-  const submitTask = async (data) => {
-    console.log(data);
-    taskList.push(data);
+  function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+
+    console.log(formJson);
+    console.log(formJson.task);
+    taskList.push(formJson.task);
+    console.log(taskList);
   };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
 
-      <form onSubmit={handleSubmit(submitTask)}>
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="task"/>
         <button type="submit">Submit</button>
       </form>
 
@@ -36,18 +33,6 @@ function App() {
         ))}
       </div>
 
-      {/* <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </div>
   );
 }
